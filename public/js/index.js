43c1805eb82921daaec1cbad5fdd49cb53084af9
Ji Yang <yangji1993@hotmail.com>
2017-01-17 17:55:30 -0800

    greet user

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,11 @@
   socket.on('connect', () => {
     console.log('connected to server');
   });
-  
+
   socket.on('disconnect', () => {
     console.log('disconnected from server');
   });
-
+ 
   socket.on('newMessage', (message) => {
     console.log('got new message:', message);
   });
