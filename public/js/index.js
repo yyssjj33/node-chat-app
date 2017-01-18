eefe57607f92bc2cd1bf777ea11e896a0c5b5dba
Ji Yang <yangji1993@hotmail.com>
2017-01-17 20:23:28 -0800

    add css for layout

--- public/js/index.js
+++ public/js/index.js
@@ -25,7 +25,7 @@ socket.on('newLocationMessage', (message) => {
 $('#message-form').on('submit', e => e.preventDefault());
 $('#send-message').on('click', (e) => {
   var input = $('[name=message]');
-  if (input.trim().length === 0) return alert('Message cannot be empty');
+  if (input.val().trim().length === 0) return alert('Message cannot be empty');
   socket.emit('createMessage', {
     from: 'User',
     text: input.val()
@@ -33,18 +33,20 @@ $('#send-message').on('click', (e) => {
     console.log('Got it', data);
     input.val('');
   });
-})
+});
 
 var locationButton = $('#send-location');
 locationButton.on('click', (e) => {
   if(!navigator.geolocation) {
     return alert('Geolocation not supported by your browser');
   }
+  locationButton.attr('disabled', 'disabled').text('Sending location ...');
   navigator.geolocation.getCurrentPosition((position) => {
     socket.emit('createLocationMessage', {
       lat: position.coords.latitude,
       lng: position.coords.longitude
     })
+    locationButton.removeAttr('disabled').text('Sending location');
   }, () => {
     return alert('Unable to fetch location');
   })
