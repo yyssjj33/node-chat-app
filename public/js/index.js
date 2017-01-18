7d137c0452225d5c10e155cacbfdc3353b104fb6
Ji Yang <yangji1993@hotmail.com>
2017-01-17 18:44:24 -0800

    ui init

--- public/js/index.js
+++ public/js/index.js
@@ -9,4 +9,20 @@
  
   socket.on('newMessage', (message) => {
     console.log('got new message:', message);
+    var li = $('<li></li>');
+    li.text(`${message.from}: ${message.text}`);
+    $('#messages').append(li);
   });
+  
+  
+
+  $('#message-form').on('submit', (e) => {
+    e.preventDefault();
+    socket.emit('createMessage', {
+      from: 'User',
+      text: $('[name=message]').val()
+    }, (data) => {
+      console.log('Got it', data);
+    });
+  })
+
--- server/server.js
+++ server/server.js
@@ -3,6 +3,8 @@ const express = require('express');
 const socketIO = require('socket.io');
 const http = require('http'); 
 
+const {generateMessage} = require('./utils/message');
+
 const publicPath = path.join(__dirname, '../public');
 const PORT = process.env.PORT || 3000;
 
@@ -15,30 +17,14 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('New user connected');
 
-  socket.emit('newMessage', {
-    from: 'Admin',
-    text: 'Welcome to the chat App',
-    createdAt: new Date().getTime()
-  });
+  socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat App'));
 
-  socket.broadcast.emit('newMessage', {
-    from: 'Admin',
-    text: 'New user joined',
-    createdAt: new Date().getTime()
-  });
+  socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined'));
 
-  socket.on('createMessage', (message) => {
-    console.log('Create:', message);
-    io.emit('newMessage', {
-      from: message.from,
-      text: message.text,
-      createdAt: new Date().getTime()
-    });
-    // socket.broadcast.emit('newMessage', {
-    //   from: message.from,
-    //   text: message.text,
-    //   createdAt: new Date().getTime()
-    // });
+  socket.on('createMessage', (message, cb) => {
+    console.log('createMessage:', message);
+    io.emit('newMessage', generateMessage(message.from, message.text));
+    cb('This is from server');
   });
 
   socket.on('disconnect', () => {
